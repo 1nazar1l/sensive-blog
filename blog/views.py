cea01f01e6074818c336c5c333506314698991fd
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-12-07 16:47:35 +0300

    replace sorted func with order_by.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
+from django.db.models import Count
+
 
 def get_related_posts_count(tag):
     return tag.posts.count()
@@ -26,15 +28,9 @@ def serialize_tag(tag):
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
-def get_likes_count(post):
-    return post.likes.count()
-
 
 def index(request):
-
-    popular_posts = Post.objects.all()
-    most_popular_posts = sorted(popular_posts, key=get_likes_count)[-5:]
-
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
